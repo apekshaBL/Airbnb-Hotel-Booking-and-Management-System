0d8cc9de3d7d1414def4d685fdf6bc1ae087023c
Apeksha Lokhande <apekshalokhande8600@gmail.com>
2025-09-06 08:57:13 +0530

    created Hotel & HotelContact entity

--- src/main/java/com/AirBnbProject/AirBnbApplication/AirBnbApplication.java
+++ src/main/java/com/AirBnbProject/AirBnbApplication/AirBnbApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AirBnbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AirBnbApplication.class, args);
+
+        SpringApplication.run(AirBnbApplication.class, args);
 	}
 
 }
