a6ce690f6643023741989dc531faabbea2589014
Apeksha Lokhande <apekshalokhande8600@gmail.com>
2025-09-08 23:13:11 +0530

    RoomRepository is created

---
+++ src/main/java/com/AirBnbProject/AirBnbApplication/dto/RoomDto.java
@@ -0,0 +1,23 @@
+package com.AirBnbProject.AirBnbApplication.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class RoomDto {
+    private long id;
+
+    private String type;
+    private BigDecimal basePrice;
+
+    private String[] photos;
+
+    private String[] amenities;
+
+    private  Integer totalCount;
+
+    private Integer capacity;
+
+
+}
