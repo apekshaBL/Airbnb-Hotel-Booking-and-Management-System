47e653ddccd6d45b18e82d8779bd57d288bdb59d
Apeksha Lokhande <apekshalokhande8600@gmail.com>
2025-09-09 07:19:12 +0530

    created Advice folder including ApiError,ApiResponse,GlobalExceptionHandler,GlobalResponseHandler classes which are used to handle the global exceptions

---
+++ src/main/java/com/AirBnbProject/AirBnbApplication/advice/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.AirBnbProject.AirBnbApplication.advice;
+
+
+import com.AirBnbProject.AirBnbApplication.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<?>>handleResourceNotFound(ResourceNotFoundException exception){
+        ApiError apiError=ApiError.builder().status(HttpStatus.NOT_FOUND).message(exception.getMessage()).build();
+        return builErrorResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>>handleInternalServerError(Exception exception){
+        ApiError apiError=ApiError.builder().status(HttpStatus.INTERNAL_SERVER_ERROR).message(exception.getMessage()).build();
+        return builErrorResponseEntity(apiError);
+    }
+
+    private ResponseEntity<ApiResponse<?>> builErrorResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(new ApiResponse<>(apiError),apiError.getStatus());
+    }
+
+
+}
